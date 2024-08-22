50eb3410b151cd829d8e0735254bfcdc64bb73b4
EmsiPi <mariecharlotte2116@gmail.com>
2024-08-22 16:12:45 +0200

    messageCreate

--- Loaders/loadEvents.js
+++ Loaders/loadEvents.js
@@ -3,7 +3,9 @@ const fs = require("fs")
 module.exports = async bot => {
     // boucle sur tous les fichiers JS contenus dans le dossier Events pour en récupérer chaque module, correspondant au nom de la commande et à sa fonction
     fs.readdirSync("./Events").filter(f => f.endsWith(".js")).forEach(async file => {
-        let command = require(`../Events/${file}`);
-        console.log(command.split(".js").join(""))
+
+        let event = require(`../Events/${file}`);
+        bot.on(file.split(".js").join(""), event.bind(null, bot));
+        console.log(`Evènement ${file} chargée avec succès !`);
     });
 }
