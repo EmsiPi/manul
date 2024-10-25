cc65dae30eaae7be0fea1afc6169486222baa872
ronanPoinsignon <ronan.poinsignon@laposte.net>
2024-10-25 21:35:30 +0200

    configuration de base pour docker et mongodb
    A ajouter dans le fichier .env :
    - MONGO_INITDB_ROOT_USERNAME (si vide, "root" par défaut)
    - MONGO_INITDB_ROOT_PASSWORD (si vide, "root" par défaut)
    - MONGO_PORT (si vide, 27017 par défaut)

--- manul.js
+++ manul.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const loadCommands = require("./Loaders/loadCommands");
 
-
 const PREFIX = "!";
 const COMMAND_MIN_LENGTH = 1;
 // pattern déterminant si l'utilisateur veut utiliser une commande ou non
@@ -9,6 +8,11 @@ const REGEX_COMMAND = "^(" + PREFIX + ")" + ".{" + COMMAND_MIN_LENGTH + ",}";
 
 const intents = new Discord.IntentsBitField(53608447);
 const bot = new Discord.Client({intents});
+
+const TOKEN = process.env.TOKEN;
+const MONGO_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const MONGO_PORT = process.env.MONGO_PORT;
 
 bot.commands = new Discord.Collection();
 bot.login(process.env.TOKEN);
@@ -51,3 +55,36 @@ function getCommandName(str) {
 	} 
 	return str.substring(PREFIX.length).split(' ')[0];
 }
+
+
+
+// partie bdd
+
+const {MongoClient, UUID} = require('mongodb');
+
+const uri = "mongodb://" + MONGO_USERNAME + ":" + MONGO_PASSWORD + "@localhost:" + MONGO_PORT;
+const mongoClient = new MongoClient(uri);
+const testDb = mongoClient.db("test")
+const testCollection = testDb.collection("test")
+
+var object1 = {
+	"_id": UUID.createFromHexString("61eb3873-4992-4364-a8c7-c6d38433143f"),
+	"test": "manul1"
+}
+var object2 = {
+	"_id": new UUID(),
+	"test": "manul2"
+}
+
+testCollection.insertMany([object1, object2]).then(async res => {
+	const findAll = testCollection.find({}).toArray();
+	findAll.then(res => {
+		console.log("find all :");
+		console.log(res);
+	})
+
+	const findOne = await testCollection.findOne({_id: UUID.createFromHexString("61eb3873-4992-4364-a8c7-c6d38433143f")});
+	console.log("find one :");
+	console.log(findOne);
+});
+
