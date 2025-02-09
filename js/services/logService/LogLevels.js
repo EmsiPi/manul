58343d71ac9aeab570c3b7ee8ad0ba4347cddd3c
ronanPoinsignon <ronan.poinsignon@laposte.net>
2025-02-09 23:12:41 +0100

    delete /js folder

--- js/services/logService/LogLevels.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const enumValue = (name) => Object.freeze({ toString: () => name });
-const Levels = Object.freeze({
-    INFO: enumValue("Info"),
-    WARNING: enumValue("Warning"),
-    ERROR: enumValue("Error")
-});
-module.exports = Levels;
